6c1e3cf1ea368d90f8f3c4444ec4055f261b9d92
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2022-04-27 12:20:47 -0700

    Update model to use tuner.

--- spongedetecttensor.py
+++ spongedetecttensor.py
@@ -14,7 +14,26 @@ def main():
     batch_size = 16
 
     training, validation = prep_data(data_path=data_path, image_size=image_size, batch=batch_size)
-    build_model(training=training, validation=validation)
+
+    tuner = kt.Hyperband(build_model,
+                            objective='val_accuracy',
+                            max_epochs=10,
+                            factor=3,
+                            directory="model",
+                            project_name="spongedetect")
+
+    tuner.search(training, epochs=2, validation_data=validation)
+    tuner.results_summary()
+
+    # model = build_model()
+    # history = model.fit(training,
+    #             shuffle=True,
+    #             validation_data=validation,
+    #             steps_per_epoch=5,
+    #             epochs=5,
+    #             validation_steps=5,
+    #             verbose=2)
+
 
 def prep_data(data_path, image_size, batch):
     training = tf.keras.preprocessing.image_dataset_from_directory(
@@ -37,29 +56,29 @@ def prep_data(data_path, image_size, batch):
     )
     return training, validation
 
-def build_model(training, validation):
-    model = keras.Sequential([
-            layers.Conv2D(32, 3, input_shape=(256, 256, 3)),
-            layers.MaxPooling2D(pool_size=2),
-            # layers.Conv2D(16, 3, input_shape=(256, 256, 3)),
-            # layers.MaxPooling2D(pool_size=2),
-            layers.Flatten(),
-            layers.Dense(4, activation='softmax'),
-            ])
+def build_model(hyper_parameters):
+    model = keras.Sequential()
+    model.add(layers.Conv2D(32, 3, input_shape=(256, 256, 3)))
+    model.add(layers.MaxPooling2D(pool_size=2))
+    model.add(layers.Conv2D(16, 3, input_shape=(256, 256, 3)))
+    model.add(layers.MaxPooling2D(pool_size=2))
 
+    if hyper_parameters.Boolean("dropout"):
+        model.add(layers.Dropout(rate=0.25))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(4, activation='softmax'))
+
+
+    learning_rate = hyper_parameters.Float("lr", min_value=1e-4, max_value=1e-2, sampling="log")
     model.compile(
-        'adam',
+        optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
         loss='sparse_categorical_crossentropy',
         metrics=['accuracy'],
     )
 
-    model.summary()
+    return model
 
-    history = model.fit(training,
-                    shuffle=True,
-                    validation_data=validation,
-                    steps_per_epoch=5,
-                    epochs=5,
-                    validation_steps=5,
-                    verbose=2)
 
+if __name__ == '__main__':
+    main()
