42bc70e4ba5f8e093fcbce612d3bb199ab2f17be
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2022-04-27 11:57:04 -0700

    Functionize the model.

--- spongedetecttensor.py
+++ spongedetecttensor.py
@@ -1,55 +1,65 @@
 from os import listdir, path
+from random import shuffle
 import numpy as np
 from skimage import io
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import keras_tuner as kt
 
+def main():
+    data_path = 'data/'
+    image_size = (256, 256)
+    batch_size = 16
 
-data_path = 'data/out/data.npz'
-data_path = 'data/'
+    training, validation = prep_data(data_path=data_path, image_size=image_size, batch=batch_size)
+    build_model(training=training, validation=validation)
 
+def prep_data(data_path, image_size, batch):
+    training = tf.keras.preprocessing.image_dataset_from_directory(
+        data_path,
+        labels="inferred",
+        validation_split=0.2,
+        subset="training",
+        seed=1337,
+        image_size=image_size,
+        batch_size=batch,
+    )
 
-image_size = (256, 256)
-batch_size = 16
+    validation = tf.keras.preprocessing.image_dataset_from_directory(
+        data_path,
+        validation_split=0.2,
+        subset="validation",
+        seed=1337,
+        image_size=image_size,
+        batch_size=batch,
+    )
+    return training, validation
 
-training = tf.keras.preprocessing.image_dataset_from_directory(
-    data_path,
-    labels="inferred",
-    validation_split=0.2,
-    subset="training",
-    seed=1337,
-    image_size=image_size,
-    batch_size=batch_size,
-)
+def build_model(training, validation):
+    model = keras.Sequential([
+            layers.Conv2D(32, 3, input_shape=(256, 256, 3)),
+            layers.MaxPooling2D(pool_size=2),
+            # layers.Conv2D(16, 3, input_shape=(256, 256, 3)),
+            # layers.MaxPooling2D(pool_size=2),
+            layers.Flatten(),
+            layers.Dense(4, activation='softmax'),
+            ])
 
-validation = tf.keras.preprocessing.image_dataset_from_directory(
-    data_path,
-    validation_split=0.2,
-    subset="validation",
-    seed=1337,
-    image_size=image_size,
-    batch_size=batch_size,
-)
+    model.compile(
+        'adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy'],
+    )
 
-model = keras.Sequential([
-        layers.Conv2D(32, 3, input_shape=(256, 256, 3)),
-        layers.MaxPooling2D(pool_size=2),
-        layers.Flatten(),
-        layers.Dense(3, activation='softmax'),
-        ])
+    model.summary()
 
-model.compile(
-    'adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['accuracy'],
-)
-
-history = model.fit(training,
-                validation_data=validation,
-                steps_per_epoch=5,
-                epochs=100,
-                validation_steps=10,
-                verbose=2)
+    history = model.fit(training,
+                    shuffle=True,
+                    validation_data=validation,
+                    steps_per_epoch=5,
+                    epochs=5,
+                    validation_steps=5,
+                    verbose=2)
 
