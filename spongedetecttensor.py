b0ae04af06ca98c65f7f3f535036e233d324236a
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2022-04-27 00:10:59 -0700

    Initial tensor model.

---
+++ spongedetecttensor.py
@@ -0,0 +1,55 @@
+from os import listdir, path
+import numpy as np
+from skimage import io
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+data_path = 'data/out/data.npz'
+data_path = 'data/'
+
+
+image_size = (256, 256)
+batch_size = 16
+
+training = tf.keras.preprocessing.image_dataset_from_directory(
+    data_path,
+    labels="inferred",
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+testing = tf.keras.preprocessing.image_dataset_from_directory(
+    data_path,
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+model = keras.Sequential([
+        layers.Conv2D(32, 3, input_shape=(256, 256, 3)),
+        layers.MaxPooling2D(pool_size=2),
+        layers.Flatten(),
+        layers.Dense(3, activation='softmax'),
+        ])
+
+model.compile(
+    'adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy'],
+)
+
+history = model.fit(training,
+                    steps_per_epoch=2,
+                    epochs=8,
+                    validation_steps=10,
+                    verbose=2)
+
+
